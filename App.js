d9b4811d2e423fbd227a0f4bdb72b693cf047378
namanh11611 <namanh11611@gmail.com>
2018-10-24 02:24:20 +0700

    Refactor code, build Bookcase app

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { createRootNavigator } from './app/router';
 
-export default class App extends React.Component {
-  render() {
-    let pic = {
-      uri: 'http://htc-wallpaper.com/wp-content/uploads/2013/10/Wolverine.jpg'
-    };
-    return (
-      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between', margin: 10}}>
-        <Image source={pic} style={{width: 45, height: 90}}/>
-        <Image source={pic} style={{width: 45, height: 90}}/>
-        <Image source={pic} style={{width: 45, height: 90}}/>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createRootNavigator();
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text>Hello NA</Text>
+//       </View>
+//     );
+//   }
+// }
