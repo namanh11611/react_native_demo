0547f9a0f90ba8ea43150467c00dd7c64ecf874a
namanh11611 <namanh11611@gmail.com>
2018-11-07 00:20:04 +0700

    Update Bookcase - Fake all screens is Bookcase screen

--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Dimensions, Platform } from 'react-native';
-import { StackNavigator, TabNavigator } from 'react-navigation';
+import { StackNavigator, createStackNavigator, createBottomTabNavigator, withNavigation, TabNavigator } from 'react-navigation';
 // import { Icon } from 'react-native-elements';
 
 import Bookcase from './screens/Bookcase';
@@ -8,6 +8,7 @@ import Explore from './screens/Explore';
 import AddBook from './screens/AddBook';
 import Lists from './screens/Lists';
 import Profile from './screens/Profile';
+import EditBook from './screens/editBook';
 
 let screen = Dimensions.get('window');
 
@@ -19,45 +20,73 @@ export const Tabs = TabNavigator({
       // tabBarIcon: ({ tintColor }) => <Icon name="open-book" type="entypo" size={28} color={tintColor} />
     },
   },
-  // 'Explore': {
-  //   screen: Explore,
-  //   navigationOptions: {
-  //     tabBarLabel: 'Explore',
-  //     tabBarIcon: ({ tintColor }) => <Icon name="ios-map-outline" type="ionicon" size={28} color={tintColor} />
-  //   },
-  // },
-  // 'Add Book': {
-  //   screen: AddBook,
-  //   navigationOptions: {
-  //     tabBarLabel: 'Add Book',
-  //     tabBarIcon: ({ tintColor }) => <Icon name="ios-add-circle-outline" type="ionicon" size={28} color={tintColor} />
-  //   },
-  // },
-  // 'Lists': {
-  //   screen: Lists,
-  //   navigationOptions: {
-  //     tabBarLabel: 'Lists',
-  //     tabBarIcon: ({ tintColor }) => <Icon name="list" type="entypo" size={28} color={tintColor} />
-  //   },
-  // },
-  // 'My Profile': {
-  //   screen: Profile,
-  //   navigationOptions: {
-  //     tabBarLabel: 'Profile',
-  //     tabBarIcon: ({ tintColor }) => <Icon name="ios-person-outline" type="ionicon" size={28} color={tintColor} />
-  //   },
-  // },
+  'Explore': {
+    screen: Bookcase,
+    navigationOptions: {
+      tabBarLabel: 'Explore',
+      // tabBarIcon: ({ tintColor }) => <Icon name="ios-map-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+  'Add Book': {
+    screen: Bookcase,
+    navigationOptions: {
+      tabBarLabel: 'Add Book',
+      // tabBarIcon: ({ tintColor }) => <Icon name="ios-add-circle-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+  'Lists': {
+    screen: Bookcase,
+    navigationOptions: {
+      tabBarLabel: 'Lists',
+      // tabBarIcon: ({ tintColor }) => <Icon name="list" type="entypo" size={28} color={tintColor} />
+    },
+  },
+  'My Profile': {
+    screen: Bookcase,
+    navigationOptions: {
+      tabBarLabel: 'Profile',
+      // tabBarIcon: ({ tintColor }) => <Icon name="ios-person-outline" type="ionicon" size={28} color={tintColor} />
+    },
+  },
+});
+
+export const BookcaseStack = createStackNavigator({
+  Bookcase: {
+      screen: Bookcase,
+      navigationOptions: ({navigation}) => ({
+          header: null,
+      }),
+  },
+  EditBook: {
+      screen: EditBook,
+      navigationOptions: ({navigation}) => ({
+          header: null,
+          tabBarVisible: false,
+          gesturesEnabled: false
+      }),
+  },
 });
 
 export const createRootNavigator = () => {
-  return StackNavigator(
+  return createStackNavigator(
     {
       Tabs: {
         screen: Tabs,
-        navigationOptions: {
-          gesturesEnabled: false
-        }
+        navigationOptions: ({ navigation }) => ({
+          gesturesEnabled: false,
+        })
+      },
+      BookcaseStack: {
+        screen: BookcaseStack,
+        navigationOptions: ({ navigation }) => ({
+          gesturesEnabled: false,
+        })
       }
+
     },
+    {
+      headerMode: "none",
+      mode: "modal"
+    }
   );
 };
